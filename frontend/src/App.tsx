341558d46251976e355e7cc6300fb83f76131435
Bruno Rafael <brunorafaelap@gmail.com>
2023-02-26 14:58:28 +0100

    fix: replace 'html' to 'div' on App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,10 @@ import Login from "./pages/login";
 
 export function App() {
   return(
-    <html data-theme="dark">
+    <div data-theme="dark">
       <NavBar />
       <Login />
       <Footer />
-    </html>
+    </div>
   )
 }
